46c323bc38bad9b0ea8017368b2601f98d358a87
kentrosewi <kentrosewi@gmail.com>
2019-05-11 16:51:35 -0500

    Heroku settings/logic

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 // define routes
 app.use('/api/card', require('./routes/api/card'));
+
+// serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
